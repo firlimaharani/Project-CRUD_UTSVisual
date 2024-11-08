2464fc0a1c17f1df5e4fed45facdb2b57c3a848f
firlimaharani <130529482+firlimaharani@users.noreply.github.com>
2024-11-08 09:36:23 +0700

    Add files via upload

---
+++ pv_biodata.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 07, 2024 at 05:28 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pv_biodata`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_biodata`
+--
+
+CREATE TABLE `tbl_biodata` (
+  `Id` int(2) NOT NULL,
+  `Nama` varchar(255) NOT NULL,
+  `Nim` varchar(12) NOT NULL,
+  `Ttl` varchar(50) NOT NULL,
+  `Jekel` varchar(50) NOT NULL,
+  `Prodi` varchar(255) NOT NULL,
+  `Alamat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
